570fc9e4fc38ec3ca6c2ea3df4e1a6f414a74d47
Aaron Sarazan <aaron@levelmoney.com>
2015-05-09 14:09:45 -0700

    Update license text

--- passive-layout/src/main/java/com/levelmoney/passivelayout/PassiveRelativeLayout.java
+++ passive-layout/src/main/java/com/levelmoney/passivelayout/PassiveRelativeLayout.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Level Money, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.levelmoney.passivelayout;
 
 import android.annotation.SuppressLint;
@@ -10,10 +26,6 @@ import android.widget.RelativeLayout;
 import com.levelmoney.passivelayout.PassiveDimensions.MeasureDelegate;
 import com.levelmoney.passivelayout.PassiveDimensions.PassiveLayoutParams;
 
-/**
- * Created by Aaron Sarazan on 7/17/14
- * Copyright(c) 2014 Level, Inc.
- */
 public class PassiveRelativeLayout extends RelativeLayout {
 
     public static final String TAG = "PassiveFillLayout";
